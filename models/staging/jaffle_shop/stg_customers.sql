24de08338f2b9e988f433e96552cff7a5e20846d
Lam Trinh <lam.trinh@rackspace.com>
2023-10-30 10:31:23 +0000

    create model dim_customers

---
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -0,0 +1,6 @@
+with
+    customers as (
+        select id as customer_id, first_name, last_name
+        from default.jaffle_shop_customers
+    )
+select * from customers
---
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -0,0 +1,6 @@
+with
+    orders as (
+        select id as order_id, user_id as customer_id, order_date, status
+        from default.jaffle_shop_orders
+    )
+select * from orders
