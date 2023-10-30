47bb938c922e77c71b8c873ef50108b17ccebc9a
Lam Trinh <lam.trinh@rackspace.com>
2023-10-30 13:31:05 +0000

    add fact orders

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -3,14 +3,15 @@ with
         select * from {{ ref('stg_customers') }}
     ),
     orders as (
-        select * from {{ ref('stg_orders') }}
+        select * from {{ ref('fct_orders') }}
     ),
     customer_orders as (
         select
             customer_id,
             min(order_date) as first_order_date,
             max(order_date) as most_recent_order_date,
-            count(order_id) as number_of_orders
+            count(order_id) as number_of_orders,
+            sum(amount) as lifetime_value
         from orders
         group by 1
     ),
@@ -21,7 +22,8 @@ with
             c.last_name,
             co.first_order_date,
             co.most_recent_order_date,
-            coalesce(co.number_of_orders, 0) as number_of_orders
+            coalesce(co.number_of_orders, 0) as number_of_orders,
+            co.lifetime_value
         from customers c
         left join customer_orders co on c.customer_id = co.customer_id
     )
